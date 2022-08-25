5546d8ab61ae6730c9a3b3e8dcc97dfbabf26123
wahidrana2108 <wahidzamanrana@gmail.com>
2022-08-25 10:34:17 +0600

    button

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
 import './Product.css';
 
 const Product = (props) => {
-    const {name, img} = props.product;
+    const {name, img, seller, price, stock} = props.product;
     return (
         <div className='product'>
            <div>
                 <img src={img} alt="" />
            </div>
            <div>
-                <h4 className='product-name'>{name}</h4>
+                <h4 className='product-name'>{name}</h4><br />
+                <p><small>By: {seller}</small></p><br />
+                <p>${price}</p><br />
+                <p><small>only {stock} left in stock - order soon</small></p>
+                <button className='main-button'> <FontAwesomeIcon icon={faShoppingCart} /> Add to cart</button>
            </div>
         </div>
     );
